2f75596349510a6b2bd1223d67ae58043f45ec45
Ma-Namba <m.namba.032016@gmail.com>
2026-07-19 15:53:04 +0900

    管理者画面の検索機能実装#4

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -15,14 +15,35 @@ class AdminController extends Controller
         $user = Auth::user();
         $categories = Category::all();
         $tags = Tag::all();
+
         $query = Contact::query();
+        $keyword = $request->input('keyword');
+        $date = $request->input('date');
+        $gender = $request->input('gender');
+        $category = $request->input('category_id');
 
-        // 検索条件がある場合（仮入れ）
-        if ($request->has('keyword')){
-            $query->where('title', 'link', '%', $request->keyword, '%');
-        }
+        $contacts = $query->when($keyword, function ($query, $keyword) {
+            return $query->where('first_name', 'like', '%' . $keyword . '%')
+                ->orwhere('last_name', 'like', '%' . $keyword . '%')
+                ->orwhere('email', 'like', '%' . $keyword . '%');
+        });
 
-        // ページネーション
+        // 日付で検索
+        $contacts = $query->when($date, function ($query, $date) {
+            return $query->where('created_at', 'like', '%' . $date . '%');
+        });
+
+        // 性別で検索
+        $contacts = $query->when($gender, function ($query, $gender) {
+            return $query->where('gender', 'like',$gender);
+        });
+
+        // カテゴリーで検索
+        $contacts = $query->when($category, function ($query, $category) {
+            return $query->where('category_id', $category);
+        });
+
+        // ページネーション（1ページに7件）
         $contacts = $query->paginate(7);
 
         return view('admin.index',compact('user','categories','contacts','tags'));
---
+++ tests/Unit/SearchTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\Category;
+use App\Models\Contact;
+use App\Models\User;
+
+class SearchTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     */
+    use RefreshDatabase;
+    public function test_example(): void
+    {
+        // コンタクトデータを10件作成
+        $category1 = Category::factory()->create();
+        $category2 = Category::factory()->create();
+        $contacts = Contact::factory()->count(5)->create([
+            'first_name' => "花子",
+            'last_name' => "鈴木",
+            'gender' => '1',
+            'category_id' => $category1->id,
+        ]);
+        $contacts = Contact::factory()->count(2)->create([
+            'first_name' => "太郎",
+            'last_name' => "佐藤",
+            'gender' => '2',
+            'category_id' => $category2->id,
+        ]);
+        $contacts = Contact::all();
+
+        // ユーザーデータを作成・ログイン
+        $user = User::factory()->create([
+            'password' => bcrypt('password123'),
+        ]);
+
+        $response = $this->post(route('login'), [
+            'email' => $user->email,
+            'password' => 'password123',
+        ]);
+
+        $response = $this->actingAs($user)->get('/admin');
+        $contacts = $response->viewData('contacts');
+
+        // 検索キーワードを指定してリクエストを送信
+        $response = $this->actingAs($user)->get('/admin?keyword=鈴木&gender=0&category_id=&date=');
+        $response->assertStatus(200);
+        $contacts = $response->viewData('contacts');
+        $this->assertCount(5, $contacts);
+
+        $response = $this->actingAs($user)->get('/admin');
+        $contacts = $response->viewData('contacts');
+
+        // カテゴリーを指定してリクエストを送信
+        $response = $this->actingAs($user)->get('/admin?keyword=&gender=0&category_id=1&date=');
+        $response->assertStatus(200);
+        $contacts = $response->viewData('contacts');
+        $this->assertCount(5, $contacts);
+
+        $response = $this->actingAs($user)->get('/admin');
+        $contacts = $response->viewData('contacts');
+
+        // 性別を指定してリクエストを送信
+        $response = $this->actingAs($user)->get('/admin?keyword=&gender=1&category_id=&date=');
+        $response->assertStatus(200);
+        $contacts = $response->viewData('contacts');
+        $this->assertCount(5, $contacts);
+
+        // 日付を指定してリクエストを送信
+        $response = $this->actingAs($user)->get('/admin?keyword=&gender=0&category_id=&date=2026-07-19');
+        $response->assertStatus(200);
+        $contacts = $response->viewData('contacts');
+        $this->assertCount(7, $contacts);
+
+    }
+}
