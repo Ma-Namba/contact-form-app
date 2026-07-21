192b65f898d10364c0a1bbfe11d4e9daae9a2dcf
Ma-Namba <m.namba.032016@gmail.com>
2026-07-21 22:26:54 +0900

    CSVエクスポート機能の実装#8

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Contact;
 use App\Models\Category;
+use App\Models\Contact;
+use App\Models\Tag;
+use App\Http\Resources\ContactResource;
 use App\Http\Requests\ContactRequest;
+use App\Http\Requests\AdminRequest;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class ContactController extends Controller
 {
@@ -26,6 +30,87 @@ class ContactController extends Controller
     {
         $validated = $request->validated();
         Contact::create($validated);
-        return view('contact.thanks',compact('validated'));
+        return view('contact.thanks', compact('validated'));
+    }
+
+        public function export(AdminRequest $request)
+    {
+        $categories = Category::all();
+
+        $validated = $request->validated();
+
+        $query = Contact::query();
+        $keyword = $validated['keyword'] ?? '';
+        $date = $validated['date'] ?? '';
+        $gender = $validated['gender'] ?? '';
+        $category = $validated['category_id'] ?? '';
+
+        // キーワードで検索
+        $contacts = $query->when($keyword, function ($query, $keyword) {
+            return $query->where('first_name', 'like', '%' . $keyword . '%')
+                ->orwhere('last_name', 'like', '%' . $keyword . '%')
+                ->orwhere('email', 'like', '%' . $keyword . '%');
+        });
+
+        // 日付で検索
+        $contacts = $query->when($date, function ($query, $date) {
+            return $query->where('created_at', 'like', '%' . $date . '%');
+        });
+
+        // 性別で検索
+        $contacts = $query->when($gender, function ($query, $gender) {
+            return $query->where('gender', 'like', $gender);
+        });
+
+        // カテゴリーで検索
+        $contacts = $query->when($category, function ($query, $category) {
+            return $query->where('category_id', $category);
+        });
+
+        $contacts = $query->with('tags')->get();
+
+        $fileName = 'contacts_export_' . date('YmdHis') . '.csv';
+
+        $headers = [
+            'Content-Type' => 'text/csv; charset=UTF-8',
+            'Content-Disposition' => 'attachment; filename="' . $fileName . '"',
+            'Pragma' => 'no-cache',
+            'Cache-Control' => 'must-revalidate, post-check=0, pre-check=0',
+            'Expires' => '0',
+        ];
+
+        return response()->stream(function () use ($contacts) {
+            // 標準出力（ダウンロードストリーム）を開く
+            $stream = fopen('php://output', 'w');
+
+            // Excelで開いたときの文字化けを防ぐ（BOMを追加）
+            fwrite($stream, pack('C*', 0xEF, 0xBB, 0xBF));
+
+            // 1行目：ヘッダー（カラム名）の書き込み
+            fputcsv($stream, ['id','category','first_name','last_name','gender','email','tel','address','building','detail','tags','created_at','updated_at']);
+
+            // 2行目以降：データの書き込み
+            foreach ($contacts as $contact) {
+                $tags_name = $contact->tags->implode('name', '、');
+                fputcsv($stream, [
+                    $contact->id,
+                    $contact->category->content,
+                    $contact->first_name,
+                    $contact->last_name,
+                    $contact->gender,
+                    $contact->email,
+                    $contact->tel,
+                    $contact->address,
+                    $contact->building,
+                    $contact->detail,
+                    $tags_name,
+                    $contact->created_at->format('Y-m-d H:i:s'),
+                    $contact->updated_at->format('Y-m-d H:i:s'),
+                ]);
+            }
+
+            fclose($stream);
+        }, 200, $headers);
+
     }
 }
